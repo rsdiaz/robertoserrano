edff154752366352ae81648b828f67cd9c8a2f69
rsdiaz <robertoserrano83@gmail.com>
2022-06-26 11:35:46 +0200

    footer

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,10 +1,10 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components'
+import { Section, FooterCopyright } from '@/components'
 
 import { AppConfig } from '@/utils/AppConfig'
 
 const Footer = () => (
   <Section>
-    <FooterCopyright site_name={AppConfig.site_name} />
+    <FooterCopyright site_name={AppConfig.site_name} author={AppConfig.author} author_link={AppConfig.author_link} />
   </Section>
 );
 

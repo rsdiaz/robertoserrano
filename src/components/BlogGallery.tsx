0fda39a093d743a57c407a3f936bb1ffdf412d90
rsdiaz <robertoserrano83@gmail.com>
2022-08-14 21:54:20 +0200

    img src

--- src/components/BlogGallery.tsx
+++ src/components/BlogGallery.tsx
@@ -1,5 +1,5 @@
 import type { MarkdownInstance } from 'astro'
-import { PostCard, Section } from '@/components'
+import { PostCard } from '@/components'
 import { Post } from '@/components/types/Post'
 
 type PostsProps = {
@@ -10,7 +10,7 @@ const BlogGallery = (props: PostsProps) => {
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {props.postList.map((post) => (
         <PostCard title={post.frontmatter.title} slug={post.frontmatter.slug} tags={post.frontmatter.tags}
-          publishDate={post.frontmatter.publishDate} />
+          publishDate={post.frontmatter.publishDate} imgSrc={post.frontmatter.imgSrc} />
       ))}
     </div>
   )

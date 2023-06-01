11cb70d17fc60ad81fa08c2fd6eadec46d2c9e9a
rsdiaz <robertoserrano83@gmail.com>
2023-06-02 01:30:51 +0200

    texto principal de la sección sobre mi

--- src/components/About/AboutMain.tsx
+++ src/components/About/AboutMain.tsx
@@ -1,13 +1,34 @@
-import Button from "../Shared/Button"
+import Button from '../Shared/Button'
 
 const AboutMain = () => (
-  <div className="flex flex-col justify-center ml-4 lg:flex-1 lg:text-left">
-    <p className="font-light leading-loose">
-      Entusiasta del desarrollo y la seguridad informática. Actualmente, trabajo como desarrollador web para una importante empresa del sector de la automoción en España. Me apasiona el desarrollo de software y la tecnología en general. Me encanta el open source y el software libre, apuesto por el conocimiento/formación libre y accesible para todos 🌍.
+  <div className='flex flex-col justify-center ml-4 lg:flex-1 lg:text-left'>
+    <h3 className='hidden mb-7 text-xl font-semibold text-primary md:text-[26px] lg:block'>
+      Una pequeña introducción
+    </h3>
+    <p className='font-light leading-loose mb-5'>
+      Desarrollador Web Full Stack con +10 de experiencia. Durante mi
+      trayectoria he tenido la oportunidad de desarrollar una amplia variedad de
+      habilidades técnicas, trabajar en una amplia gama de proyectos y ayudar a
+      las empresas a alcanzar sus metas.
     </p>
-    <div className="flex justify-center pt-8 gap-6 md:justify-start">
-      <Button variant='btn-primary' href='#skill'>Skills</Button>
-      <Button variant='btn-outlined' href='#portfolio'>Portfolio</Button>
+    <p className='font-light leading-loose mb-5'>
+      A lo largo de mi carrera, siempre he priorizado el aprendizaje constante y
+      la adaptación a las nuevas tecnologías y tendencias. Siempre estoy
+      buscando formas de mejorar mis habilidades y expandir mi conocimiento.
+      Esta mentalidad de crecimiento ha sido crucial para mantenerme al día en
+      un campo que cambia rápidamente 🌍.
+    </p>
+    <p>
+      Actualmente estoy trabajando para una importante empresa del sector de la
+      automoción en España.
+    </p>
+    <div className='flex justify-center pt-8 gap-6 md:justify-start'>
+      <Button variant='btn-primary' href='#skill'>
+        Skills
+      </Button>
+      <Button variant='btn-outlined' href='#portfolio'>
+        Portfolio
+      </Button>
     </div>
   </div>
 )

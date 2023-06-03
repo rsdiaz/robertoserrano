1ec5db368c12a319ac4f81c2da612b2b0271399c
rsdiaz <robertoserrano83@gmail.com>
2023-06-04 01:06:53 +0200

    titulo, descripción y navegación de secciones

--- src/components/BlogGallery.tsx
+++ src/components/BlogGallery.tsx
@@ -3,13 +3,34 @@ import { Post } from '@/components/types/Post'
 import { BlogCard } from './Blog'
 import { Card } from './Shared/Card'
 import { Colors } from '@/utils/Colors'
+import { PortfolioNav } from './PortfolioNav'
+import { useState } from 'react'
+import { SectionNav } from './Shared/SectionNav'
 
 type PostsProps = {
   postList: MarkdownInstance<Post>[]
 }
 const BlogGallery = (props: PostsProps) => {
+  const [skillType, setSkillType] = useState('javascript')
+  const tags = ['javascript', 'typescript']
+
   return (
     <section>
+      <h2 className='hidden mb-7 text-xl font-semibold text-primary md:text-[26px] lg:block'>
+        Últimos artículos publicados en el blog
+      </h2>
+      <p className='font-light leading-loose mb-5'>
+        Estos son los últimos, puedes visitar el blog para consultar todo el
+        archivo de posts.
+      </p>
+      <div className='flex justify-end pb-6'>
+        <SectionNav
+          prev='Portfolio'
+          next='Contacto'
+          prevHref='#portfolio'
+          nextHref='#blog'
+        />
+      </div>
       <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
         {props.postList.map((post) => (
           <BlogCard
